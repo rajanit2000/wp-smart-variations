1e25c14eeee74ddb9750bf6d02a8267f6a9a255b
rajanit2000 <rajanit2000@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-13 04:42:10 +0000

    Upload New
    git-svn-id: https://plugins.svn.wordpress.org/wp-smart-variations/trunk@1006280 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ install.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: WP Smart Variations
+Author: <strong>Rajan V</strong>
+Version: 1.0
+Description: This is variations plugin for WP-e Commerce Site.
+*/
+
+/*  Copyright 2007-2014 Rajan V (email: ratanit2000 at gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// Hook for adding admin menus
+add_action('wp_footer', 'variationScript');
+add_action('wp_head', 'whshCSS');
+
+function whshCSS(){
+	echo '<link type="text/css" rel="stylesheet" href="'.plugins_url().'/wp-smart-variations/css/custom.css" />';
+}
+
+function variationScript() {
+    ?>
+	<script type="text/javascript" >
+	jQuery(function(){
+		jQuery('.wpsc_variation_forms').append('<div class="new-variation-div"></div>');
+		var i = 0;
+		jQuery( ".wpsc_variation_forms" ).each(function() {
+			var output = '';
+			var variation = '';
+			jQuery(this).find('.wpsc_select_variation').each(function(){
+				var name = jQuery(this).attr('name');
+					jQuery(this).find('option').each(function(){
+						var val = jQuery(this).text();
+						var value = jQuery(this).attr('value');
+						if( value != '0' ){
+							variation = variation + '<label class="new-variation-radio radio-variation" for="'+name+i+'"><input class="wpsc_select_variation" type="radio" id="'+name+i+'" name="'+name+'" value="'+value+'" rel=' + val +' />'+val+'</label>';
+						}
+						i++;
+					});
+					variation = '<div class="variation-variation">'+variation+'</div>';
+				output = variation;
+			});
+			jQuery(this).find('.new-variation-div').html( output );
+			jQuery(this).find('table').hide();
+		});
+	});
+    </script>
+	<?php
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== WP Smart Variations ===
+Contributors: rajanit2000
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2MS5UWJSWJZYJ
+Tags:  WP-Commerce change variations, wp-ecommerce, variations, variations checkbox, variations radio, product variations, ecommerce variations
+Requires at least: 3.0
+Tested up to: 4.0
+Stable tag: 4.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is WP eCommerce variations Plugin for WP eCommerce Site. Its use to WP-Commerce change variations from selects to inputs
+
+== Description ==
+
+This is WP eCommerce variations Plugin for WP eCommerce Site. Its use to swap select box to radio buttons for Product variations
+
+= Custom Options =
+	
+	1. Install Plugin
+	2. No need short code
+	3. No need functions
+
+= Make me a beer =
+
+If you like this plugin [Donate](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2MS5UWJSWJZYJ)
+
+== Installation ==
+
+	1. Its very simple Install and Active
+	
+== Frequently Asked Questions ==
+
+1. Is this plugin working without WP eCommerce Plugin?
+
+	No, WP eCommerce Plugin is required for this plugin
+
+== Screenshots ==
+
+1. screenshot-1.png 
+
+== Changelog ==
+
+= 1.0 =
+
+* New: WP eCommerce Variation Plugin
+
+== Upgrade Notice ==
+
+The current version of WP Smart Wishlist requires WordPress 3.0 or higher. If you use older version of WordPress, you need to upgrade WordPress first.
